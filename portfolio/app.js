7c28e27449a02bdb8cf93b8bae93039262454d8a
Tarikuzuma <edwinjrgumba@gmail.com>
2024-01-10 04:56:51 +0800

    Added navbar

--- portfolio/app.js
+++ portfolio/app.js
@@ -30,5 +30,18 @@ hiddenElements.forEach(element => {
   observer.observe(element);
 });
 
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {
+  scrollFunction()
+};
 
-
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("navbar").style.top = "0";
+    document.querySelector('.nav_main').classList.add('greenBackground');
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+    document.querySelector('.nav_main').classList.remove('greenBackground');
+  }
+}
