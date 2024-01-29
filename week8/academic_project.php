000327583663b5df0fa1dfd509661a022fefda2e
Tarikuzuma <edwinjrgumba@gmail.com>
2024-01-29 22:22:11 +0800

    Added First Academic Project

--- week8/academic_project.php
+++ week8/academic_project.php
@@ -57,18 +57,25 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
                       <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
                     </svg>           
-                  Project 1 
+                  Fursuit of Justice! (2022)
                   </a>
                 </div>
 
                 <div class="about">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies aliquam,
-                    nunc nisi aliquet nunc, vitae aliquam nunc nisi quis nunc. Nullam auctor, nisl eget ultricies aliquam,
-                    nunc nisi aliquet nunc, vitae aliquam nunc nisi quis nunc. Nullam auctor, nisl eget ultricies aliquam,
-                    nunc nisi aliquet nunc, vitae aliquam nunc nisi quis nunc. Nullam auctor, nisl eget ultricies aliquam,
-                    nunc nisi aliquet nunc, vitae aliquam nunc nisi quis nunc. Nullam auctor, nisl eget ultricies aliquam,
-                    nunc nisi aliquet nunc, vitae aliquam nunc nisi quis nunc. Nullam auctor, nisl eget ultricies aliquam,
-                    nunc nisi aliquet nunc, vitae aliquam nunc nisi quis nunc.
+                    Fursuit of Justice is a Visual Novel Game created in Renpy, a Visual Novel Engine. Team Wright’s general goal is to educate people 
+                    about the justice system and the existing laws that come with it in a way that can be understood by people outside legal professions.The gameplay and main storyline is typically
+                    created in a script that has over 5000 lines of code. The game is a parody of the Ace Attorney Series, and is a game that I created
+                    with my groupmates in the Bootcamp 2022. I was the Lead Programmer of the game, and I was also the one who modified bits of the
+                    main storyline of the game. The game is currently unfinished, but maybe more content will come someday. <br>
+                    <br>
+
+                    <ol>
+                      <li>Jaden Mae Ong - Lead Artist & Story Writer</li>
+                      <li>Edwin Gumba - Lead Programmer & Story Writer</li>
+                      <li>Kai Butalid - Lead UI/UX & Documentation</li>
+                      <li>Omar Decena - Sub Programmer & Documentation</li>
+                    </ol>
+
                 </div>
 
                 
@@ -78,13 +85,25 @@
                 <button class="carousel-button next" data-carousel-button="next">&#8658;</button>
                 <ul data-slides>
                   <li class="slide" data-active>
-                    <img src="https://source.unsplash.com/78A265wPiO4" alt="Nature Image #1">
+                    <img src="assets/projects/academic_projects/proj1/1.JPG" alt="Game Screenshot 1">
                   </li>
                   <li class="slide">
-                    <img src="https://source.unsplash.com/eOpewngf68w" alt="Nature Image #2">
+                    <img src="assets/projects/academic_projects/proj1/2.JPG" alt="Game Screenshot 2">
                   </li>
                   <li class="slide">
-                    <img src="https://source.unsplash.com/ndN00KmbJ1c" alt="Nature Image #3">
+                    <img src="assets/projects/academic_projects/proj1/3.JPG" alt="Game Screenshot 3">
+                  </li>
+                  <li class="slide">
+                    <img src="assets/projects/academic_projects/proj1/4.JPG" alt="Game Screenshot 4">
+                  </li>
+                  <li class="slide">
+                    <img src="assets/projects/academic_projects/proj1/5.JPG" alt="Game Screenshot 5">
+                  </li>
+                  <li class="slide">
+                    <img src="assets/projects/academic_projects/proj1/6.JPG" alt="Game Screenshot 6">
+                  </li>
+                  <li class="slide">
+                    <img src="assets/projects/academic_projects/proj1/7.JPG" alt="Game Screenshot 7">
                   </li>
                 </ul>
               </div>
