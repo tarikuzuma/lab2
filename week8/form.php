e8edf0f0847e403f5e565ef78bee1895bc095c1c
Tarikuzuma <edwinjrgumba@gmail.com>
2024-02-01 15:09:23 +0800

    Added forms.html

---
+++ week8/form.php
@@ -0,0 +1,204 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Contact</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type = "text/css" href = "css/cursor.css">
+    <script defer src = "js/app.js"></script>
+
+    <!-- Connect to LastFM API -->
+    <script type="text/javascript" src="js/last_fm/lastfm.api.cache.js"></script>
+    <script type="text/javascript" src="js/last_fm/lastfm.api.js"></script>
+    <script type="text/javascript" src="js/last_fm/lastfm.api.md5.js"></script>
+    <script type="text/javascript" src="js/last_fm/last_fm_connector.js"></script>
+</head>
+<body>
+
+
+    <!--Navigation Bar-->
+    <header class = "nav_main">
+      <nav class = "navigation">
+        <div id="navbar">
+          <a href="home.php">Home</a>
+          <a href="projects.php">Projects</a>
+          <a href="hobbies.php">Hobbies</a>
+          <a href="contact.php">Contact</a>
+        </div>
+      </nav>
+    </header>
+
+    <!--Script for Mouse Cursos-->
+    <div class = "cursor-dot" data-cursor-dot></div>
+    <div class = "cursor-outline" data-cursor-outline></div>
+    <script src = "js/cursor.js"></script>
+
+    <section class = "hidden" style = "margin-top: 100px">
+    
+        <?php
+        // define variables and set to empty values
+        $nameErr = $emailErr = $genderErr = $websiteErr = "";
+        $name = $email = $gender = $comment = $website = "";
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+            $nameErr = "Name is required";
+        } else {
+            $name = test_input($_POST["name"]);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+            $nameErr = "Only letters and white space allowed";
+            }
+        }
+        
+        if (empty($_POST["email"])) {
+            $emailErr = "Email is required";
+        } else {
+            $email = test_input($_POST["email"]);
+            // check if e-mail address is well-formed
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
+            }
+        }
+            
+        if (empty($_POST["website"])) {
+            $website = "";
+        } else {
+            $website = test_input($_POST["website"]);
+            // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+            $websiteErr = "Invalid URL";
+            }
+        }
+
+        if (empty($_POST["comment"])) {
+            $comment = "";
+        } else {
+            $comment = test_input($_POST["comment"]);
+        }
+
+        if (empty($_POST["gender"])) {
+            $genderErr = "Gender is required";
+        } else {
+            $gender = test_input($_POST["gender"]);
+        }
+        }
+
+        function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+        }
+        ?>
+
+        <h2>PHP Form Validation Example</h2>
+        <p><span class="error">* required field</span></p>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+        Name: <input type="text" name="name" value="<?php echo $name;?>">
+        <span class="error">* <?php echo $nameErr;?></span>
+        <br><br>
+        E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+        <span class="error">* <?php echo $emailErr;?></span>
+        <br><br>
+        Website: <input type="text" name="website" value="<?php echo $website;?>">
+        <span class="error"><?php echo $websiteErr;?></span>
+        <br><br>
+        Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+        <br><br>
+        Gender:
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+        <span class="error">* <?php echo $genderErr;?></span>
+        <br><br>
+        <input type="submit" name="submit" value="Submit">  
+        </form>
+
+        <?php
+        echo "<h2>Your Input:</h2>";
+        echo $name;
+        echo "<br>";
+        echo $email;
+        echo "<br>";
+        echo $website;
+        echo "<br>";
+        echo $comment;
+        echo "<br>";
+        echo $gender;
+        ?>
+
+    </section>
+
+<footer style = "margin-top: 100px; margin-bottom: 50px;">
+
+        <div class = "row">
+  
+          <div class = "col">
+            <div class = "col">
+  
+              <div class = "spotify" style = "width: 669px">
+                <div class = "container">
+                  <div class = "image">
+                    <img src="assets/spotify.png">
+                  </div>
+                    <div class = "texts" 
+                    style= "position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    justify-content: center;
+                    text-align: center;">
+                      <div class = "now">
+                        Now Listening to:
+                      </div>
+                      <div id = "most_played_song">
+                        <!-- Temporary-->
+                        Teddy Picker
+                      </div>
+                      <div id = "most_played_author">
+                        <!-- Temporary-->
+                        by Arctic Monkeys     
+                      </div>
+                    </div>
+                </div>
+              </div>
+  
+            </div>
+            <div class = "col" style = "padding-left: 40px; padding-top: 30px;">
+            2023 . Edwin Gumba 
+            </div>
+          </div>
+          <div class ="col">
+            <div class ="col">
+              Applications
+            </div>
+            <div class = "col">
+              LinkedIn
+            </div>
+            <div class = "col">
+              GitHub
+            </div>
+            <div class = "col">
+              Discord
+            </div>
+            <div class = "col">
+              Vlogs
+            </div>
+          </div>
+  
+          <div class = "col">
+            <div class = "col">
+              Contact
+            </div>
+            <div class = "col">
+              Emails
+            </div>
+          </div>
+        </div>
+  
+        <hr class = "line">
+  
+      </footer>
+
+</body>
+</html>
