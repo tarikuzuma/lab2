9877ebf2f85675f4dda80831000a6ac8de5058c4
Tarikuzuma <edwinjrgumba@gmail.com>
2024-02-02 14:20:40 +0800

    Fixed reference list

--- week8/references.php
+++ week8/references.php
@@ -6,25 +6,27 @@
 <body>
     
     <h1>
-        <a href="https://www.seanhalpin.xyz/about">Inspiration of website</a>
+        <a href="https://www.seanhalpin.xyz/about">Inspiration for Website</a>
     </h1>
 
     <h2>References for Creation of Webpage:</h2>
-    https://stackoverflow.com/questions/12813573/position-icons-into-circle <br>
-    https://www.sitepoint.com/using-the-last-fm-api/ <br>
-    https://www.youtube.com/watch?v=T33NN_pPeNI <br>
-    https://www.youtube.com/watch?v=UMdvufdewD8 <br> <br>
+    <ul>
+        <li><a href="https://stackoverflow.com/questions/12813573/position-icons-into-circle">Position Icons into Circle</a></li>
+        <li><a href="https://www.sitepoint.com/using-the-last-fm-api/">Using the Last.fm API</a></li>
+        <li><a href="https://www.youtube.com/watch?v=T33NN_pPeNI">YouTube Tutorial: Building a Webpage</a></li>
+        <li><a href="https://www.youtube.com/watch?v=UMdvufdewD8">YouTube Tutorial: Another Web Development Tutorial</a></li>
+    </ul>
 
-    Card <br>
-    https://codepen.io/carlita712/pen/mNLzye <br>
+    <p>Card: <a href="https://codepen.io/carlita712/pen/mNLzye">Card Example</a></p>
 
-    Icons
-    https://icons.getbootstrap.com/ <br>
+    <p>Icons: <a href="https://icons.getbootstrap.com/">Bootstrap Icons</a></p>
 
-    Images <br>
-    https://cdn-developer-wp.arc.dev/wp-content/uploads/2022/06/coding-programming-project-ideas-1128x635.jpg
+    <p>Image Source: <a href="https://cdn-developer-wp.arc.dev/wp-content/uploads/2022/06/coding-programming-project-ideas-1128x635.jpg">Coding Project Ideas Image</a></p>
 
-    Carousel <br>
-    https://github.com/WebDevSimplified/js-css-carousel
+    <p>Carousel: <a href="https://github.com/WebDevSimplified/js-css-carousel">JS/CSS Carousel</a></p>
+
+    <p>PHP Tutorial: <a href="https://dev.to/anthonys1760/how-to-insert-form-data-into-a-database-using-html-php-2e8">Insert Form Data into Database Using HTML and PHP</a></p>
+
+
 </body>
 </html>
