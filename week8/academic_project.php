a0fd003b02cf455c3fe66994018e45bf5bc90622
Tarikuzuma <edwinjrgumba@gmail.com>
2024-02-14 19:43:14 +0800

    Finalized Academic Projects

--- week8/academic_project.php
+++ week8/academic_project.php
@@ -53,7 +53,7 @@
             
             <div class="container">
                 <div class="proj_title">
-                  <a class = "github_link" href = "https://github.com/tarikuzuma/RamFit">
+                  <a class = "github_link" href = "https://www.figma.com/file/cPJ3TtyKfCwDOoV0Oh2KV2/Synth-Dev?type=design&node-id=805784%3A449&mode=design&t=0UfsDXLUNcTJLRjv-1">
                     <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
                       <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
                     </svg>           
@@ -72,10 +72,8 @@
                  benefit the user.  <br>
                     <br>
 
-                    Access the user manual through this <a class = "github_link" href = "https://drive.google.com/file/d/1_56FBFA4oKwXCU_6mdEd1w7AJrOjbb0T/view?usp=sharing"> link </a> as well! <br><br>
+                    Access the user interface through this <a class = "github_link" href = "https://www.figma.com/proto/cPJ3TtyKfCwDOoV0Oh2KV2/Synth-Dev?type=design&node-id=805828-894&t=Yy4ha1ZE5oOhYGMP-1&scaling=scale-down&page-id=805784%3A449&starting-point-node-id=805828%3A894&show-proto-sidebar=1&mode=design"> link </a> as well! <br><br>
 
-
-                    Languages: Python <br>
                     Softwares Used: Figma, PyQt Designer, Visual Studio Code<br>
 
                     <ol>
@@ -94,19 +92,25 @@
                 <button class="carousel-button next" data-carousel-button="next">&#8658;</button>
                 <ul data-slides>
                   <li class="slide" data-active>
-                    <img src="assets/projects/academic_projects/proj4/5.png" alt="Game Screenshot 5">
+                    <img src="assets/projects/academic_projects/proj6/1.JPG" alt="Game Screenshot 5">
                   </li>
                   <li class="slide">
-                    <img src="assets/projects/academic_projects/proj4/2.JPG" alt="Game Screenshot 2">
+                    <img src="assets/projects/academic_projects/proj6/2.JPG" alt="Game Screenshot 2">
                   </li>
                   <li class="slide">
-                    <img src="assets/projects/academic_projects/proj4/4.png" alt="Game Screenshot 4">
+                    <img src="assets/projects/academic_projects/proj6/3.JPG" alt="Game Screenshot 4">
                   </li>
                   <li class="slide">
-                    <img src="assets/projects/academic_projects/proj4/1.JPG" alt="Game Screenshot 1">
+                    <img src="assets/projects/academic_projects/proj6/4.JPG" alt="Game Screenshot 1">
                   </li>
                   <li class="slide">
-                    <img src="assets/projects/academic_projects/proj4/3.png" alt="Game Screenshot 3">
+                    <img src="assets/projects/academic_projects/proj6/5.JPG" alt="Game Screenshot 3">
+                  </li>
+                  <li class="slide">
+                    <img src="assets/projects/academic_projects/proj6/6.JPG" alt="Game Screenshot 3">
+                  </li>
+                  <li class="slide">
+                    <img src="assets/projects/academic_projects/proj6/7.JPG" alt="Game Screenshot 3">
                   </li>
                 </ul>
               </div>
